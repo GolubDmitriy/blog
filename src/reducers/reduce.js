adfd8935656c66eae2a307d13b39daea7181c5cf
Dmitriy <golub.d.a@yandex.ru>
2019-06-04 13:40:52 +0300

    Добавлен reduce.js

---
+++ src/reducers/reduce.js
@@ -0,0 +1,16 @@
+const initialState = [];
+
+const reducer = (state=initialState, action) => {
+
+    switch (action.type) {
+        case 'POSTS_LOADED':
+            return {
+                books: action.payload
+            };
+        default:
+            return state;
+    }
+
+}
+
+export default reducer;

8a7eaa58bb95535cb8a3245a93373a4c3c7a3597
Dmitriy <golub.d.a@yandex.ru>
2019-06-05 12:59:32 +0300

    В store добавил поле для одного конкретного поста

--- src/reducers/reduce.js
+++ src/reducers/reduce.js
@@ -1,5 +1,6 @@
 const initialState = {
-    posts: []
+    posts: [],
+    post: {}
 };
 
 const reducer = (state=initialState, action) => {
@@ -7,12 +8,19 @@ const reducer = (state=initialState, action) => {
     switch (action.type) {
         case 'POSTS_LOADED':
             return {
+                ...state,
                 posts: action.payload
             };
         case 'DELETE_POST':
             return {
+                ...state,
                 posts: state.posts.filter(post => post.id !== action.payload)
-            }
+            };
+        case 'POST_LOADED':
+            return {
+                ...state,
+                post: action.payload
+            };
         default:
             return state;
     }

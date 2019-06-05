f8d61f2684c26fc0bb91f97b546e0dc369200994
Dmitriy <golub.d.a@yandex.ru>
2019-06-05 16:27:21 +0300

    Добавлена ссылка на страницу с постами в header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <div>
+            <h1>Header</h1>
+            <Link to="/posts/">Posts</Link>
+        </div>
     )
 }
 

8833fd8f85167e024e2beb281b431d156cd1240e
Dmitriy <golub.d.a@yandex.ru>
2019-06-06 12:17:05 +0300

    NewPost преобразован в компонент класс

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const NewPost = () => {
-    return (
-        <div>
-            <form onSubmit={(event) => {
-                    event.preventDefault()
-                    console.log(666)}}>
-                <input type="text" placeholder="title" />
-                <input type="text" placeholder="body" />
-                <input type="submit" />
-            </form>
-        </div>
-    )
+class NewPost extends React.Component {
+    
+    state = {
+        valueTitle: '',
+        valueBody: '' 
+    }
+
+    sendNewPost = event => {
+        event.preventDefault();
+        console.log(this.state.valueBody, this.state.valueTitle)
+    }
+
+    changeValueTitle = event => {
+        this.setState({
+            valueTitle: event.target.value
+        })
+    }
+
+    changeValueBody = event => {
+        this.setState({
+            valueBody: event.target.value
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={ this.sendNewPost }>
+                    <input 
+                        type="text" 
+                        placeholder="title" 
+                        value={ this.state.valueTitle }
+                        onChange={ this.changeValueTitle } />
+                    <input 
+                        type="text" 
+                        placeholder="body" 
+                        value={ this.state.valueBody }
+                        onChange={ this.changeValueBody } />
+                    <input type="submit" />
+                </form>
+            </div>
+        )
+    }
 }
 
-export default NewPost;
+export default connect()(NewPost);

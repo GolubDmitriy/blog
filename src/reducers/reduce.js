e052e75281baaf294b1f9a9afb47a34e8f8bf199
Dmitriy <golub.d.a@yandex.ru>
2019-06-06 15:59:48 +0300

    Добавлен reduce для редактирования поста

--- src/reducers/reduce.js
+++ src/reducers/reduce.js
@@ -30,6 +30,17 @@ const reducer = (state=initialState, action) => {
         case 'ADD_NEW_POST':
             state.posts.push(action.payload)
             return state;
+        case 'EDIT_POST':
+            console.log(action.payload)
+            return {
+                ...state,
+                posts: state.posts.map(post => {
+                    if (post.id === Number(action.payload.id)) {
+                        return action.payload;
+                    }
+                    return post;
+                })
+            };
         default:
             return state;
     }

b4cccdd2d728a106e1ff1be8849653dca1d3b73e
Dmitriy <golub.d.a@yandex.ru>
2019-06-06 11:37:56 +0300

    В header добавлена ссылка на страницу создания нового поста

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => {
         <div>
             <h1>Header</h1>
             <Link to="/posts/">Posts</Link>
+            <Link to="/new-post">New Post</Link>
         </div>
     )
 }

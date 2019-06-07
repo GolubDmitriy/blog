1e20617b9f2afb0903b0ff54bf2270330ded5d44
Dmitriy <golub.d.a@yandex.ru>
2019-06-07 16:15:23 +0300

    Компонент NewComment теперь компонент класс

---
+++ src/components/NewComment/NewComment.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NewComment extends React.Component {
+
+    state = {
+        newCommentValue: ''
+    }
+
+    render() {
+        return (
+            <h1>NewComment</h1>
+        )
+    }
+
+}
+
+export default NewComment;

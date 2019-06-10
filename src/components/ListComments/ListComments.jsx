7241d875a00d3e6f865a4d725ddbfe9933e8c491
Dmitriy <golub.d.a@yandex.ru>
2019-06-10 11:56:43 +0300

    Компонент ListComments стал функциональным компонентом

--- src/components/ListComments/ListComments.jsx
+++ src/components/ListComments/ListComments.jsx
@@ -2,56 +2,34 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
-import { commentsLoaded } from '../../actions/actions'
-
-import ApiServices from '../../services/apiServices';
-
 import Comment from '../Comment/Comment';
 
-class ListComments extends React.Component {
-    
-    componentDidMount() {
-        const apiServices = new ApiServices();
-        apiServices.getCommentsByPostId(this.props.postId)
-            .then(comments => {
-                this.props.commentsLoaded(comments);
-            })
-    }
-    
-    render() {
+const ListComments = ({ comments, postId }) => {
+ 
+    const commentsByPost = comments.filter(comment => Number(comment.postId) === Number(postId));
 
-        const { comments } = this.props;
-
-        const commentsList = comments.map(comment => {
-            return (
-                <li key={ comment.id }>
-                    <Comment 
-                        name={ comment.name }
-                        email={ comment.email }
-                        body={ comment.body }
-                    />
-                </li>
-            )
-        })
-
+    const commentsList = commentsByPost.map(comment => {
         return (
-            <ul>
-                { commentsList }
-            </ul>
+            <li key={ comment.id }>
+                <Comment 
+                    name={ comment.name }
+                    email={ comment.email }
+                    body={ comment.body }
+                />
+            </li>
         )
-    }
+    })
+
+    return (
+        <ul>
+            { commentsList }
+        </ul>
+    )
 }
+
 
 const mapStateToProps = ({ comments }) => {
     return  { comments }; 
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        commentsLoaded: comments => {
-            dispatch(commentsLoaded(comments))
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ListComments);
+export default connect(mapStateToProps)(ListComments);

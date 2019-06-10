4ed9904f9f1a82a6b6b86d3f4522e036c8007b05
Dmitriy <golub.d.a@yandex.ru>
2019-06-10 16:01:52 +0300

    К Header подключен компонент FilterBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import FilterBar from '../FilterBar/FilterBar'
+
 import './Header.css';
 
 const Header = () => {
@@ -10,6 +12,7 @@ const Header = () => {
             <h1>Header</h1>
             <Link to="/posts/">Posts</Link>
             <Link to="/new-post">New Post</Link>
+            <FilterBar />
         </div>
     )
 }

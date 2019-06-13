c5906e5f26fb02135eb6ab2e8a5ae86f8c0f024d
Dmitriy <golub.d.a@yandex.ru>
2019-06-13 13:15:18 +0300

    Добавлена тень для блока поста

--- src/components/ListPosts/ListPosts.jsx
+++ src/components/ListPosts/ListPosts.jsx
@@ -70,7 +70,7 @@ const ListsPosts = ({
     const elements = visiblePosts.map(post => {
         const commentsPost = comments.filter(comment => Number(post.id) === Number(comment.postId))
         return (
-            <li key={ post.id }>
+            <li key={ post.id } className="shadow p-3 mb-5 bg-light rounded" >
                 <ListItemPost
                     idPost = { post.id } 
                     title={ post.title }

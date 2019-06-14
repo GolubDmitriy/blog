76bccf9c12810c42cfc9fae3c55ad38dcf7b92cb
Dmitriy <golub.d.a@yandex.ru>
2019-06-14 16:16:06 +0300

    Добавлена возможность удаления комментария

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
-const Comment = ({ email, name, body }) => {
+import { connect } from 'react-redux';
+
+import { deleteComment } from '../../actions/actions';
+
+const Comment = ({ email, name, body, deleteComment, idComment }) => {
     return (
         <div>
             <h5 className="mb-1 text-center">{ name }</h5>
             <p>{ email }</p>
             <p className="mb-1">{ body }</p>
+            <span 
+                onClick={() => deleteComment(idComment)}
+                className="btn btn-outline-dark">
+                Удалить комментарий
+            </span>
         </div>
     )
 }
 
-export default Comment;
+const mapDispatchToProps = dispatch => {
+    return {
+        deleteComment: idComment => {
+            dispatch(deleteComment(idComment))
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Comment);

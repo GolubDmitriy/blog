4231a3f0cce7556c83138a69cd2851382407f54f
Dmitriy <golub.d.a@yandex.ru>
2019-06-14 15:09:47 +0300

    Добавлен новый компонент Error5xxx

---
+++ src/components/Error5xx/Error5xx.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error5xx = () => {
+    return (
+        <div className="alert alert-danger" role="alert">
+            <strong>Упс...</strong> Не можем подгрузить посты, попробуйте обновить страницу или зайти позже.
+        </div>
+    )
+}
+
+export default Error5xx;

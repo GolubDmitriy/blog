dbaf1ae0f94dc7f8a1689080e76a46cc87387c70
Dmitriy <golub.d.a@yandex.ru>
2019-06-14 15:45:00 +0300

    Добавлен новый компонент NotFoundPost

---
+++ src/components/NotFoundPost/NotFoundPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPost = () => {
+    return (
+        <div class="alert alert-danger" role="alert">
+            <strong>Упс...</strong> Не удалось найти этот пост.
+        </div>
+    )
+}
+
+export default NotFoundPost;

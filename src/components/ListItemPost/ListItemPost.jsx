d42ab8695d44f1d3244ace2ae8f3142fcfeb3866
Dmitriy <golub.d.a@yandex.ru>
2019-06-14 11:45:18 +0300

    Добавлен компонент InstrumentBar

--- src/components/ListItemPost/ListItemPost.jsx
+++ src/components/ListItemPost/ListItemPost.jsx
@@ -1,51 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 
-import BlockComments from '../BlockComments/BlockComments';
-
-import { deletePost, setLike, setDislike } from '../../actions/actions';
+import InstrumentBar from '../InstrumentBar/InstrumentBar';
 
 import './ListItemPost.css';
 
 class ListItemPost extends React.Component { 
-     
-    state = {
-        visibileComments: false,
-    }
-
-    changeVisibleComments = () => {
-        this.setState(prevState => {
-            return {
-                visibileComments: !prevState.visibileComments
-            }
-        })
-    }
 
     render() {
         const {
             title, 
             text, 
             idPost, 
-            deletePost, 
-            setLike, 
             like, 
-            setDislike, 
-            dislike, 
-            comments } = this.props;
-
-        const likeBtn = 
-            like ? 
-            (<span onClick={() => setLike(idPost)} className="btn btn-success">Like</span>) :
-            (<span onClick={() => setLike(idPost)} className="btn btn-outline-success">Like</span>)
-
-        const dislikeBtn = 
-            dislike ? 
-            (<span onClick={() => setDislike(idPost)} className="btn btn-danger">Dislike</span>) :
-            (<span onClick={() => setDislike(idPost)} className="btn btn-outline-danger">Dislike</span>)
-
-        const blockComments = this.state.visibileComments ? <BlockComments postId={ idPost } /> : null
-                                 
+            dislike 
+            } = this.props;
+                   
         return (
             <div className="post-mini">
                 <div className="post-mini-title">
@@ -58,37 +28,13 @@ class ListItemPost extends React.Component {
                 <div className="post-mini-body">
                     { text }
                 </div>
-                <div className="post-mini-menu-bar navbar">
-                    { likeBtn }
-                    { dislikeBtn }
-                    <span>About</span>
-                    <button onClick={ this.changeVisibleComments }
-                        className="btn btn-outline-primary">
-                        Comments { comments }
-                    </button>
-                    <span onClick={() => deletePost(idPost)}
-                        className="btn btn-outline-dark">
-                        Delete
-                    </span>
-                </div>
-                { blockComments }
+                <InstrumentBar 
+                        idPost={ idPost } 
+                        like= { like} 
+                        dislike={ dislike } />
             </div>
         )
     };
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        deletePost: idPost => {
-            dispatch(deletePost(idPost))
-        },
-        setLike: idPost => {
-            dispatch(setLike(idPost))
-        },
-        setDislike: idPost => {
-            dispatch(setDislike(idPost))
-        }
-    }
-}
-
-export default connect(null, mapDispatchToProps)(ListItemPost);
+export default ListItemPost;

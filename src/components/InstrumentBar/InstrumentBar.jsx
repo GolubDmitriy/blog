87690176bb34e378697015c11f9e8fcb902469fb
Dmitriy <golub.d.a@yandex.ru>
2019-06-14 11:45:45 +0300

    Создан новый компонент InstrumentBar

---
+++ src/components/InstrumentBar/InstrumentBar.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { connect } from 'react-redux'; 
+
+import { deletePost, setLike, setDislike } from '../../actions/actions';
+
+import BlockComments from '../BlockComments/BlockComments';
+
+class InstrumentBar extends React.Component {
+    
+    state = {
+        visibleComments: false
+    }
+
+    changeVisibleComments = () => {
+        this.setState(prev => {
+            return {
+                visibileComments: !prev.visibileComments
+            }
+        })
+    }
+
+    render() {
+
+        const {
+            idPost, 
+            deletePost, 
+            setLike,  
+            setDislike, 
+            comments,
+            like,
+            dislike
+            } = this.props;
+
+        const blockComments = this.state.visibileComments ? <BlockComments postId={ idPost } /> : null
+
+        const commentsPost = comments.filter(comment => Number(idPost) === Number(comment.postId))
+
+        return (
+            <React.Fragment>
+                <div className="post-mini-menu-bar navbar">
+                    <span 
+                        onClick={() => setLike(idPost)} 
+                        className={ like ? "btn btn-success" : "btn btn-outline-success"}>
+                        Like
+                    </span>
+                    <span 
+                        onClick={() => setDislike(idPost)} 
+                        className={ dislike ? "btn btn-danger" : "btn btn-outline-danger"}>
+                        Dislike
+                    </span>
+                    <button 
+                        onClick={ this.changeVisibleComments }
+                        className="btn btn-outline-primary">
+                        Comments { commentsPost.length }
+                    </button>
+                    <span 
+                        onClick={() => deletePost(idPost)}
+                        className="btn btn-outline-dark">
+                        Delete
+                    </span>
+                </div>
+                { blockComments }
+            </React.Fragment>
+
+        )
+    }
+}
+
+const mapStateToProps = ({ comments }) => {
+    return {  comments }
+} 
+
+const mapDispatchToProps = dispatch => {
+    return {
+        deletePost: idPost => {
+            dispatch(deletePost(idPost))
+        },
+        setLike: idPost => {
+            dispatch(setLike(idPost))
+        },
+        setDislike: idPost => {
+            dispatch(setDislike(idPost))
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(InstrumentBar);

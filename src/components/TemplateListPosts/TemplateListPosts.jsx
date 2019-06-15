9441daf2578dff976a2d5d7c5f280416ae16562a
Dmitriy <golub.d.a@yandex.ru>
2019-06-15 13:50:36 +0300

    Добавил оповещение пользователю при поиске

--- src/components/TemplateListPosts/TemplateListPosts.jsx
+++ src/components/TemplateListPosts/TemplateListPosts.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import VisibleBar from '../VisibleBar/VisibleBar';
 import FilterBar from '../FilterBar/FilterBar';
@@ -6,13 +7,29 @@ import SearchBar from '../SearchBar/SeacrhBar';
 
 import './TemplateListPosts.css';
 
-const TemplateListPosts = ({ elements, resultPosts=[] }) => {
+const TemplateListPosts = ({ elements=null, resultPosts=[], searchQuery, foundPosts, isSeacrh }) => {
+    
+    const searchMessageFound = (
+        <div class="alert alert-dark" role="alert">
+            По запросу <strong>{ searchQuery }</strong> найдено: { foundPosts.length } 
+        </div>
+    )
+
+    const searchMessageNotFound = (
+        <div class="alert alert-dark" role="alert">
+            По запросу <strong>{ searchQuery }</strong> ничего не найдено
+        </div>
+    )
+
+    const searchMessage = isSeacrh && foundPosts.length !== 0 ? searchMessageFound : searchMessageNotFound
+    
     return (
         <React.Fragment>
-            <div className="d-flex justify-content-between">
+            <div className="d-flex justify-content-between template-list-posts">
                 <FilterBar />
                 <SearchBar />
             </div>
+            { searchQuery ? searchMessage : null }
             <ul className="list-group list-posts">
                 { elements }
             </ul>
@@ -21,4 +38,8 @@ const TemplateListPosts = ({ elements, resultPosts=[] }) => {
     )
 }
 
-export default TemplateListPosts;
+const mapStateToProps = ({ foundPosts, searchQuery, isSeacrh }) => {
+    return { foundPosts, searchQuery, isSeacrh }
+}
+
+export default connect(mapStateToProps)(TemplateListPosts);

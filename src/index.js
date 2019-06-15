03f17416bea01a5d7c7971976aee9f4f521c54ec
Dmitriy <golub.d.a@yandex.ru>
2019-06-15 16:41:40 +0300

    Все приложение обернуто в компонент ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 ReactDOM.render(
-    <Provider store={ store }>
-        <App />
-    </Provider>, document.getElementById('root')
+    <ErrorBoundary>
+        <Provider store={ store }>
+            <App />
+        </Provider>
+    </ErrorBoundary>, document.getElementById('root')
 );

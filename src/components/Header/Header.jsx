0a91b66307ce28f6e5fd1ecea90aa3dc09756533
unknown <golub.d.a@yandex.ru>
2019-06-16 19:44:00 +0300

    Отцентровал ссылки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,9 +15,11 @@ const Header = ({ resetAllFilter, removeSearchStatus }) => {
     
     return (
         <nav className="nav">
-            <h1 className="nav-link">Header</h1>
-            <Link to="/posts/" onClick={ reset } className="nav-link">Posts</Link>
-            <Link to="/new-post" className="nav-link">New Post</Link>
+            <h1 className="nav-link text-info">Блог</h1>
+            <div className="d-flex align-self-center">
+                <Link to="/posts/" onClick={ reset } className="nav-link">Посты</Link>
+                <Link to="/new-post" className="nav-link">Создать пост</Link>
+            </div>
         </nav>
     )
 }

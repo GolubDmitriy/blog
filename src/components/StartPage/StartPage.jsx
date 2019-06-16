9dbd11145f6055012ef830899dbda7866b53378c
unknown <golub.d.a@yandex.ru>
2019-06-16 19:36:50 +0300

    Изменен вид стартовой страницы

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const StartPage = () => {
-    return <h1>Start page</h1>
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-title">Добро пожаловать!</h4>
+                <h6 className="card-subtitle mb-2 text-muted">Тематический блог</h6>
+                <p className="card-text">
+                Вы можете просматривать посты других пользоватлей, так и писать свои,
+                оценивать их и оставлять свои комментарии.
+                </p>
+                <Link to="/posts/" className="card-link">Читать посты</Link>
+                <Link to="/new-post" className="card-link">Создать свой пост</Link>
+            </div>
+        </div>
+    )
 }
 
 export default StartPage;
